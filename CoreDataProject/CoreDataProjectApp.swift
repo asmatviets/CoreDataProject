7698a142e1f24897becab4c3593b01af5868a8dc
AndreyMatviets <asmatviets@gmail.com>
2023-02-06 10:14:42 +0400

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Andrey Matviets on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
